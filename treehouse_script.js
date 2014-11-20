57edc448d2cf4b216c8bb8a670959b24d11ada0d
papermashea <molloy.shea@gmail.com>
2014-11-19 21:17:36 -0500

    adding comments

--- treehouse_script.js
+++ treehouse_script.js
@@ -1,10 +1,15 @@
+//comments will teach me things
 console.log("hello from your inner voice");
 console.log(".....bitch");
 
+
+//mantra from today "slow is good"
 var name = prompt("who the hell are you?");
 
 alert("alrighty "+ name);
 
 name = ".....whatever. it doesn't matter. nothing matters."
 
+
+//this is just for practice
 console.log("the user claims to be " + name);
