99fca4c260c4a70f19ab594180f38229a93b9aac
papermashea <molloy.shea@gmail.com>
2014-11-19 20:51:21 -0500

    adding practice files. potentially unprofessional

---
+++ treehouse_script.js
@@ -0,0 +1,10 @@
+console.log("hello from your inner voice");
+console.log(".....bitch");
+
+var name = prompt("who the hell are you?");
+
+alert("alrighty "+ name);
+
+name = ".....whatever. it doesn't matter. nothing matters."
+
+console.log("the user claims to be " + name);
